ae0347a69275de226f11dd6192e56185548691f8
raj <hmacha.jscript@gmail.com>
2020-08-28 15:53:20 +0530

    repos logic is in progress

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { Info, Repos, User, Search, Navbar } from '../components';
+import loadingImage from '../images/preloader.gif';
+import { GithubContext } from '../context/context';
+import { MdRestaurant } from 'react-icons/md';
+
+const Dashboard = () => {
+  const data = useContext(GithubContext);
+  console.log(data)
+  return (
+    <main>
+      <Navbar />
+      <Search />
+      <Info />
+      <User />
+      <Repos />
+    </main>
+  );
+};
+
+export default Dashboard;
